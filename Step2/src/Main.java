796b8290f1ce2d0389e53bec0cef91f30645293f
Alex Pelaez <pelaezalex8@gmail.com>
2020-03-26 13:45:24 -0400

    Finished step3 code

--- Step2/src/Main.java
+++ Step2/src/Main.java
@@ -5,24 +5,23 @@ import org.antlr.v4.runtime.tree.*;
 import java.io.File;
 import java.io.FileInputStream;
 public class Main {
-
+//    fails: 5,6,7,8,11,13,16,18,20,21
     public static void main(String[] args) throws Exception {
-        String filePath = "/Users/Alex/Documents/School/CSCI468-Compilers/Step1/inputs/test1.micro";
-//        String filePath = args[0];
+//        String filePath = "/Users/Alex/Documents/School/CSCI468-Compilers/Step2/src/inputs/test7.micro";
+        String filePath = args[0];
         File file = new File(filePath);
         FileInputStream inputStream = new FileInputStream(file);
         ANTLRInputStream input = new ANTLRInputStream(inputStream);
         LittleGLexer lexer = new LittleGLexer(input);
-        LittleGLexer lexer1 = new LittleGLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         LittleGParser parser = new LittleGParser(tokens);
         parser.removeErrorListeners();
         ParseTree tree = parser.program();
 
 //        System.out.println(tree.toStringTree(parser));
-        
+
         int numErrors = parser.getNumberOfSyntaxErrors();
-        System.out.println(numErrors);
+//        System.out.println(numErrors);
         if(numErrors != 0){
             System.out.println("Not accepted\n");
 
---
+++ Step3/src/Main.java
@@ -0,0 +1,47 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.util.*;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final String GLOBAL = "GLOBAL";
+        final String NAME = "name ";
+        final String TYPE = " type ";
+        final String DECLARATION_ERROR = "DECLARATION ERROR";
+        final String ERROR = "error";
+        final String ST = "Symbol table ";
+//        String filePath = "/Users/Alex/Documents/School/CSCI468-Compilers/Step3/src/inputs/test21.micro";
+        String filePath = args[0];
+        File file = new File(filePath);
+        FileInputStream inputStream = new FileInputStream(file);
+        ANTLRInputStream input = new ANTLRInputStream(inputStream);
+        LittleGLexer lexer = new LittleGLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LittleGParser parser = new LittleGParser(tokens);
+        Listener listener = new Listener();
+        new ParseTreeWalker().walk(listener, parser.program());
+        LinkedHashMap<String, LinkedHashMap<String, String>> symbolTable = listener.getSymbolTable();
+
+        try {
+            String errorMessage = symbolTable.get(DECLARATION_ERROR).get(ERROR);
+            System.out.println(DECLARATION_ERROR + " " + errorMessage);
+        } catch (Exception e) {
+            symbolTable.forEach((key, value) -> {
+                if(key != GLOBAL)
+                    System.out.println();
+
+                System.out.println(ST + key);
+                value.forEach((name, type) -> {
+                    System.out.println(NAME + name + TYPE + type);
+                });
+            });
+        }
+
+
+    }
+}
+
+
